90c0170b0b568b8424fe361528ff758981889708
Buddha-naveka <162609305+Buddha-naveka@users.noreply.github.com>
2024-11-16 11:12:50 +0400

    Add files via upload

--- Lab5.py
+++ Lab5.py
@@ -8,8 +8,7 @@ import timeit
 perem, fact1, fact2 = 1 ,2, 2
 while perem ==1:
     n = int(input('Введите n: '))
-    choice = int(input('1.Рекурсивно \n2.Итерационно \n'))
-    
+    choice = int(input('1.Рекурсивно \n2.Итерационно \n'))  
     if choice ==1: 
         def factre(n): #Функция для вычисления факториала вместо неё используется функция библиотееки math
             if n == 1 or n == 0:
@@ -23,7 +22,10 @@ while perem ==1:
             elif n == 1:
                 return 2
             else:
-                return (-1)**n * ((Fr(n-1)/math.factorial(n)) - 2*(Fr(n-2)) /math.factorial(2*n))
+                if n%2==0:
+                    return 1 * ((Fr(n-1)/math.factorial(n)) - 2*(Fr(n-2)) /math.factorial(2*n))
+                else:
+                    return (-1) * ((Fr(n-1)/math.factorial(n)) - 2*(Fr(n-2)) /math.factorial(2*n))
                 #return (-1)**n * (Fr(n-1)/factre(n) - 2*Fr(n-2) /factre(2*n))
 
         a =timeit.timeit('Fr(n)', globals = globals(), number =1)
@@ -31,18 +33,23 @@ while perem ==1:
         
     elif choice ==2:
         def Fi(n, fact1,fact2):
+            two, one = 1,2
             for i in range(0,n+1):
                 if i == 0:
                     answer = 1
-                    two = answer
                 elif i == 1:
                     answer = 2
-                    one = answer
                 else:
-                    fact2 = fact2 * (i*2) * ((i*2)-1)
-                    answer = (-1)**i * (one/fact1 - 2*two /fact2)
-                    two, one = one, answer
-                    fact1 *=(i+1)            
+                    if i%2==0:
+                        fact2 = fact2 * (i*2) * ((i*2)-1)
+                        answer = 1 * (one/fact1 - 2*two /fact2)
+                        two, one = one, answer
+                        fact1 *=(i+1) 
+                    else:      
+                        fact2 = fact2 * (i*2) * ((i*2)-1)
+                        answer = (-1) * (one/fact1 - 2*two /fact2)
+                        two, one = one, answer
+                        fact1 *=(i+1)      
             return answer
 
         a =timeit.timeit('Fi(n, fact1, fact2)', globals = globals(), number =1)
