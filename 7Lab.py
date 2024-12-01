d0e4f4f8347de5a960b3582598ede53c99274d42
Buddha-naveka <162609305+Buddha-naveka@users.noreply.github.com>
2024-12-02 02:00:37 +0400

    Add files via upload

--- 7Lab.py
+++ 7Lab.py
@@ -5,6 +5,10 @@ from itertools import permutations
 
 
 #fruits = []
+'''def tim(fruits):
+    b =list(permutations(fruits,7))
+    #a =timeit.timeit('b', globals = globals(), number =1)
+    return b'''
 def click_button():
  
     fruits = []
@@ -27,19 +31,57 @@ def click_button():
                 btn["state"] = ["disabled"]
                 fruits.append(probel[i])
                 if len(fruits) == 10:
-                    b =list(permutations(fruits,3))
-                    #print(b)
                     window2 = tk.Tk()
                     window2.title('Ответ')
                     window2.geometry('500x450+650+200')
                     window2.configure(bg = '#4682B4')
                     t = tk.Text(window2)
                     t.place(x =60, y = 70, width= 400, height= 250)
-                    t.insert("1.0", b) 
                     scrollbar = tk.Scrollbar(window2,orient="vertical", command=t.yview)
                     scrollbar.place(x = 460, y = 70, height= 250)
                     #scrollbar.pack( side = 'right', fill='y' )
+
                     t["yscrollcommand"]=scrollbar.set
+                    #-------------------------------------------------------------------------
+                    min1, sum = 10000, 0
+                    h, naim, spisok=[], [], []
+                    sum2  = 0
+                    for i in range(len(fruits)):
+                        for o in range(len(fruits)):
+                            if o != i:
+                                for p in range(len(fruits)):
+                                    if p != i and p!=o:
+                                        for j in range(len(fruits)):
+                                            if j != i and j!=o and j!=p:
+                                                for k in range(len(fruits)):
+                                                    if k != i and k!=o and k!=p and k!=j:
+                                                        for l in range(len(fruits)):
+                                                            if l != i and l!=o and l!=p and l!=j and l!=k:
+                                                                for m in range(len(fruits)):
+                                                                    if m != i and m!=o and m!=p and m!=j and m!=k and m!=l:
+                                                                        sum = 0
+                                                                        spisok.append(fruits[i]); sum += sugar[i] * 1 
+                                                                        spisok.append(fruits[o]); sum += sugar[o] * 2
+                                                                        spisok.append(fruits[p]); sum += sugar[p] * 3
+                                                                        spisok.append(fruits[j]); sum += sugar[j] * 4
+                                                                        spisok.append(fruits[k]); sum += sugar[k] * 5
+                                                                        spisok.append(fruits[l]); sum += sugar[l] * 6
+                                                                        spisok.append(fruits[m]); sum += sugar[m] * 7
+                                                                        sum2 +=1
+                                                                        if sum < min1:
+                                                                            naim =[]
+                                                                            naim=spisok
+                                                                            min1 = sum
+                                                                        h.append(spisok)
+                                                                        spisok =[]
+
+                    b =list(permutations(fruits,7))
+                    a =timeit.timeit(lambda: list(permutations(fruits, 7)), globals = globals(), number =1)
+                    tekst = f"Количиство комбинаций: {sum2} \nКомбинация с наименьшей сахарностью: {naim} \nЕё сахарность{sum} \nВремя: {a} \nНесколько вариантов комбинаций{b[:100]}"
+                    #--------------------------------------------------------------------
+                    
+                    #t.insert("1.0", b[:100]) 
+                    t.insert('end', tekst) 
                     window2.mainloop()
                     
     else:
